945fb9e7b6b57b4b3bed3d9cb0694b6325399d99
Andreas <aka@jma.dk>
2023-03-08 14:01:53 +0100

    create impl and struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::process::{Command};
+use std::{str, env};
+//use serde::{Serialize, Deserialize};
+// use serde_json::{Result};
+struct Device {
+
+}
+
+impl Device {
+    fn from_utf8(s: &[u8]) -> Result<String, std::str::Utf8Error> {
+        match String::from_utf8(s.to_vec()) {
+            Ok(v) => Ok(v),
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let output = Command::new("iwctl").args(["device", "list"]).output().expect("failed to execute process");
+    println!("{:?}", output);
 }
